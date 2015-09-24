13576662465f052245a772e582a28ea8174469ce
Alexa M <nataly.mx@outlook.com>
2015-09-24 17:40:40 -0500

    Iniciando Relax Movil

---
+++ public/index.php
@@ -0,0 +1,202 @@
+<?php $urlrelax = "http://www.revistarelax.com/";?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Revista Relax</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0">
+    <meta name="keywords" content="Revista Relax">
+    <link href="css/my-style.css" rel="stylesheet" type="text/css">
+
+    <script type="text/javascript" rel="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+    <script src="js/jquery.min.js"></script>
+
+    <!-- menu-->
+    <script src="js/scripts.js"></script>
+
+    <script type="application/x-javascript">addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); }</script>
+    <!-- slide-->
+    <script src="js/responsiveslides.min.js"></script>
+    <script>
+      $(function () {
+      $("#slider").responsiveSlides({
+      auto: true,
+      speed: 500,
+      namespace: "callbacks",
+      pager: true,
+      });
+      });
+    </script>
+  </head>
+  <body>
+    <!--Navegación-->
+    <div class="navigation">
+      <div class="container-fluid">
+        <nav class="pull">
+          <ul>
+            <li><a href="index.html">Relax Hoy</a></li>
+            <li><a href="about.html">Revista Relax</a></li>
+            <li><a href="blog.html">Infografías</a></li>
+            <li><a href="terms.html">Congresos</a></li>
+            <li><a href="privacy.html">Blog</a></li>
+            <li><a href="contact.html">Entrevistas</a></li>
+            <li><a href="contact.html">Multimedia</a></li>
+            <li><a href="contact.html">Editorial</a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+
+    <!-- Header -->
+    <header>
+      <!-- Cabecera -->
+      <h1 class="title">Relax - Lectura Terapeútica</h1>
+      <div class="magazine-button"><i class="fa fa-2x fa-leanpub"></i></div>
+      <div class="logo"><img src="images/Revista-Relax.png" alt="Revista Relax"></div>
+      <div class="nav_slide_button"><a href="#"><i class="fa fa-2x fa-bars"></i></a></div>
+    
+      <!--Buttons Bar Menu -->
+        <div class="bottom-header">
+          <div class="header-bottom">
+              
+            <div class="bottom-head">
+              <a href="buy.html">
+                <div class="buy-media">
+                 <i class="buy"> </i>
+                 <h6>Buy</h6>
+               </div>
+              </a>
+            </div>
+
+            <div class="bottom-head">
+              <a href="buy.html">
+                <div class="buy-media">
+                 <i class="rent"> </i>
+                 <h6>Buy</h6>
+               </div>
+              </a>
+            </div>
+            
+            <div class="bottom-head">
+              <a href="buy.html">
+                <div class="buy-media">
+                 <i class="pg"> </i>
+                 <h6>Buy</h6>
+               </div>
+              </a>
+            </div>
+
+            <div class="bottom-head">
+              <a href="buy.html">
+                <div class="buy-media">
+                 <i class="sell"> </i>
+                 <h6>Buy</h6>
+               </div>
+              </a>
+            </div>
+
+            <div class="bottom-head">
+              <a href="buy.html">
+                <div class="buy-media">
+                 <i class="loan"> </i>
+                 <h6>Buy</h6>
+               </div>
+              </a>
+            </div>
+
+            <div class="bottom-head">
+              <a href="buy.html">
+                <div class="buy-media">
+                 <i class="apart"> </i>
+                 <h6>Buy</h6>
+               </div>
+              </a>
+            </div>
+            
+            <div class="bottom-head">
+              <a href="buy.html">
+                <div class="buy-media">
+                 <i class="deal"> </i>
+                 <h6>Buy</h6>
+               </div>
+              </a>
+            </div>
+
+            <div class="clearfix"></div>
+
+        </div>
+          </div>
+
+
+
+
+      <!-- Slider -->
+      <div class="slider">
+        
+            <ul class="rslides" id="slider">           
+             
+             <li>
+                <div class="banner banner1">
+                  <div class="caption">
+                    <h3>Estamos en <span>Twitter</span></h3>
+                    <p>@RelaxAtenea</p>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <div class="banner banner2">
+                  <div class="caption">
+                    <h3>Revista Relax <span>Digital</span></h3>
+                    <p>Ya está disponible</p>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <div class="banner banner3">
+                  <div class="caption">
+                    <h3><span>Infografías </span> de interés</h3>
+                    <p>Visite nuestra sección especial</p>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <div class="banner banner4">
+                  <div class="caption">
+                    <h3><span>Anúnciese </span> en Relax</h3>
+                    <p>Publicidad Efectiva Tel. (55) 5575-1100</p>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <div class="banner banner5">
+                  <div class="caption">
+                    <h3><span>Conozca </span> Relax</h3>
+                    <p>Una revista para médicos</p>
+                  </div>
+                </div>
+              </li>
+
+            </ul>
+            <div class="callbacks_container"></div>
+        </div>
+
+
+
+        
+
+
+
+
+    </header>
+
+
+
+        
+
+
+
+  </body>
+</html>
